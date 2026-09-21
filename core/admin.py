d61f85df1a3f635b1e2a1ce6141b5e27bb61cf97
akira61 <otaku.fahad11@gmail.com>
2026-09-22 01:06:04 +0300

    admin panel for users using unflod style

--- core/admin.py
+++ core/admin.py
@@ -1,4 +1,42 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from .models import User
+from unfold.admin import ModelAdmin
+from unfold.forms import AdminPasswordChangeForm, UserChangeForm, UserCreationForm
 
-admin.site.register(User)
+@admin.register(User)
+class UserAdmin(BaseUserAdmin, ModelAdmin):
+    model = User
+    form = UserChangeForm
+    add_form = UserCreationForm
+    change_password_form = AdminPasswordChangeForm
+
+    list_display = (
+        # "id",
+        "username",
+        "email",
+        "first_name",
+        "last_name",
+        "last_login",
+        "is_active",
+        "is_staff",
+        "is_superuser",
+        'date_joined'
+    )
+
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        ("Personal info", {"fields": ('first_name', 'last_name', 'email')}),
+        ("Permissions", {"fields": ("is_active", "is_staff", "is_superuser", "groups", "user_permissions")}),
+        ("Important dates", {"fields": ("last_login", "date_joined")}),
+    )
+
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("username", "email", "usable_password", "password1", "password2"),
+            },
+        ),
+    )
